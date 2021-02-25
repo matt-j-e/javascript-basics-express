9a85a30a98d478c3e277cb4da1adda7dfebb527d
mattedwards <the.matt.edwards@googlemail.com>
2021-02-25 15:54:08 +0000

    added lowercase function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const { sayHello, uppercase } = require('./lib/strings');
+const { sayHello, 
+        uppercase,
+        lowercase,
+        firstCharacter } = require('./lib/strings');
 
 const app = express();
 
@@ -16,6 +19,10 @@ app.get('/strings/hello/:string', (req, res) => {
 
 app.get('/strings/upper/:string', (req, res) => {
   res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string) });
 });
 
 module.exports = app;
